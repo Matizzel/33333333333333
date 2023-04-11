1e7b8c32592a02651c3cb8fce92a719c4a3b92db
matiz <matiz@MATIZZELLAPTOP>
2023-04-11 12:32:43 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace _33333333333333
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double f(double x)
+            {
+                return Math.Pow(x, 2);
+            }
+            {
+                double x = Convert.ToDouble(textBox1.Text);
+                double y = Convert.ToDouble(textBox2.Text);
+                double z = Convert.ToDouble(textBox3.Text);
+                double r = Math.Max(Math.Min(f(x), y), z);
+
+                textBox4.Text = r.ToString();
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
